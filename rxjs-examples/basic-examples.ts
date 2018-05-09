f481fb436ff98764eb47234729f08613ace05666
hevans90 <hevans9000@gmail.com>
2018-05-09 17:47:29 +0100

    feat: add rxjs examples

---
+++ rxjs-examples/basic-examples.ts
@@ -0,0 +1,20 @@
+import { of, forkJoin } from 'rxjs';
+import { concat } from 'rxjs/operators';
+
+console.log('\nRxJs Examples\n');
+
+// emits 1,2,3
+const sourceOne = of(1, 2, 3);
+
+// emits 4,5,6
+const sourceTwo = of(4, 5, 6);
+
+// concat: emit values from sourceOne, when complete, subscribe to sourceTwo
+const example1 = sourceOne.pipe(concat(sourceTwo));
+console.log('Basic concat:');
+const subscribe1 = example1.subscribe(val => console.log(val));
+
+// forkJoin: return an array of the latest values from each stream
+const example2 = forkJoin(sourceOne, sourceTwo);
+console.log('Basic forkJoin:');
+const subscribe2 = example2.subscribe(val => console.log(val));
---
+++ rxjs-examples/transformation-examples.ts
@@ -0,0 +1,32 @@
+import { of } from 'rxjs';
+import { switchMap, mergeMap, delay } from 'rxjs/operators';
+
+console.log('\nRxJs Examples\n');
+
+// emits 1,2,3
+const sourceOne = of(1, 2, 3).pipe(delay(2000));
+
+// emits 4,5,6
+const sourceTwo = of(4, 5, 6).pipe(delay(1000));
+
+// mergeMap: emit payloads from inner observable when outer emits
+const example3 = sourceOne.pipe(
+  mergeMap(val => {
+    console.log(`outer: ${val}`);
+    return sourceTwo;
+  }),
+);
+console.log('Basic mergeMap:');
+const subscribe3 = example3.subscribe(val => console.log(`inner: ${val}`));
+
+// switchMap: emit payloads from outer observable until inner emits, at which point cancel outer
+setTimeout(() => {
+  const example4 = sourceOne.pipe(
+    switchMap(val => {
+      console.log(`outer: ${val}`);
+      return sourceTwo;
+    }),
+  );
+  console.log('\nBasic switchMap:');
+  const subscribe4 = example4.subscribe(val => console.log(`inner: ${val}`));
+}, 2000);
